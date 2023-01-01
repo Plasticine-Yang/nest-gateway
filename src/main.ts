1c7a7edff841dc1884d9587eb4c99da7a1fe8fce
plasticine9750 <975036719@qq.com>
2023-01-01 14:23:30 +0800

    feat: integrate with logger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify'
+import { NestFastifyApplication } from '@nestjs/platform-fastify'
 
 import { AllExceptionFilter } from './common/filters/base.exception.filter'
 import { HttpExceptionFilter } from './common/filters/http.exception.filter'
 import { TransformInterceptor } from './common/interceptors/transform.interceptor'
+import { fastifyAdaptor } from './common/fasify'
 
 import { AppModule } from './app.module'
 
@@ -16,7 +14,7 @@ import { setupDocument } from './docs'
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    fastifyAdaptor,
   )
 
   // 接口版本化管理
